03182c0848870360701d7f72ec207acc0dd44a61
Thol Chidambaram <tholkappia@gmail.com>
2018-11-17 21:42:28 -0800

    added some tests

--- lab9/decorator/Tester.java
+++ lab9/decorator/Tester.java
@@ -4,9 +4,13 @@ public class Tester {
 
     public static void runTest()
     {
-        Component obj = new ConcreteDecoratorB( new ConcreteDecoratorA( new ConcreteComponent() ) ) ;
+        Component obj     = new ConcreteDecoratorB( new ConcreteDecoratorA( new ConcreteComponent() ) ) ;
+        Component origObj = new ConcreteComponent() ;
+        Component objAB   = new ConcreteDecoratorA( new ConcreteDecoratorB( new ConcreteComponent() ) ) ;
         String result = obj.operation() ;
         System.out.println( result );
+        System.out.println( origObj.operation() );
+        System.out.println( objAB.operation() );
     }
 
   public static void main( String [] args )
